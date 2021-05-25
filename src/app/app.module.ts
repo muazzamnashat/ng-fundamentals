6e6b4b0b6f74764c139ef387be3cdd9045385c6c
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-05-25 15:39:54 -0400

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent],
+  bootstrap: [EventsAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <nav-bar> </nav-bar>
+    <events-list></events-list>
+  `,
 })
-export class AppComponent {
+export class EventsAppComponent {
   title = 'ng-fundamentals';
 }
